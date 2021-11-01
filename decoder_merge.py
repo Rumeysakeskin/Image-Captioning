19d7a49807d77976125b988eada7f22dc3ed0b26
Rumeysakeskin <61134521+Rumeysakeskin@users.noreply.github.com>
2021-11-01 10:30:27 +0300

    Update decoder_merge.py

--- decoder_merge.py
+++ decoder_merge.py
@@ -17,24 +17,18 @@ class Decoder(nn.Module):
 
         self.softmax = nn.LogSoftmax(dim=1)
 
-
+# Merge
     def forward(self, input, hidden, feature):
 
         feature = self.map(feature)
-
         output = self.embedding(input)
-
         output = F.relu(output)
-
         output, hidden = self.gru(output, hidden)
-
         output = torch.cat((feature, output), dim=2)
-
         output = self.softmax(self.out(output[0]))
 
 
         return output, hidden
-
 
 def get_decoder(feature_size=2048, output_size=10000, embed_size=128, num_layers=1):
     return Decoder(feature_size=feature_size, output_size=output_size, embed_size=embed_size, num_layers=num_layers)
